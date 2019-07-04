853f8006d40b55d3670b07e2ccbe195c1af8f733
Siddhartha Bhagwan <siddharthabhagwan@gmail.com>
2019-07-04 14:47:58 -0400

    Updates

---
+++ precache-manifest.b765e34e4491647ce98d511a696a3d49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee63d1d297151546090a3f4d153e45a",
+    "url": "/ebc-dashboard/index.html"
+  },
+  {
+    "revision": "7426de9ecfe3206447f1",
+    "url": "/ebc-dashboard/static/css/main.7c421f3c.chunk.css"
+  },
+  {
+    "revision": "88d73103c474ec1ff6a6",
+    "url": "/ebc-dashboard/static/js/2.ba108c4a.chunk.js"
+  },
+  {
+    "revision": "7426de9ecfe3206447f1",
+    "url": "/ebc-dashboard/static/js/main.b4707b3c.chunk.js"
+  },
+  {
+    "revision": "9833424da3c3132432b3",
+    "url": "/ebc-dashboard/static/js/runtime~main.4ca6d89e.js"
+  }
+]);
--- src/store/store.tsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers, createStore } from "redux";
-import { detailsReducer } from "../reducers/detailsReducer";
-
-export const store = createStore(
-  combineReducers({
-    details: detailsReducer
-  })
-);
